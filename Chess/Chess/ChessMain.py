3b76ea66de5444dc689c70db504f68a999f6c931
VHN1305 <vanhuyngo135@gmail.com>
2022-12-03 03:42:18 +0700

    Now, 2 player can play this game

--- Chess/Chess/ChessMain.py
+++ Chess/Chess/ChessMain.py
@@ -8,22 +8,28 @@ SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
 IMAGES = {}
 
-def loadImage ():
+
+def loadImage():
     pieces = ["bR", "bN", "bB", "bQ", "bK", "bp", "wR", "wN", "wB", "wQ", "wK", "wp"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("image/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
+
 def main():
+
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
-    p.display.set_caption("Chess _ Ngo Van Huy 20204657 _ IT1 _ 05 _ K65 _ RHUST")
+    p.display.set_caption("Chess _ Ngo Van Huy 20204657 _ IT1 _ 05 _ K65 _ HUST")
     clock = p.time.Clock()
-    screen.fill(p.Color("white"))
     gs = ChessEngine.gameState()
+    validMoves = gs.getValidMoved()
+    moveMade = False
+
     loadImage()
     running = True
     sqSelected = ()
     playerClicks = []
+
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -38,39 +44,69 @@ def main():
                 else:
                     sqSelected = (row, col)
                     playerClicks.append(sqSelected)
-                    # p.draw.rect(screen, p.Color("blue"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
                 if len(playerClicks) == 2:
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
-                    sqSelected = ()
-                    playerClicks = []
+                    for i in range(len(validMoves)):
+                        if move == validMoves[i]:
+                            gs.makeMove(validMoves[i])
+                            moveMade = True
+                            sqSelected = ()
+                            playerClicks = []
 
-        drawGameState(screen,gs)
+                    if not moveMade:
+                        playerClicks = [sqSelected]
+
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    moveMade = True
+
+        if moveMade:
+            validMoves = gs.getValidMoved()
+            moveMade = False
+
+        drawGameState(screen, gs, validMoves, sqSelected)
         clock.tick(MAX_FPS)
         p.display.flip()
 
 
+def hightlightSquares (screen, gs, validMoves, sqSelected):
+    if sqSelected != ():
+        r, c = sqSelected
+        if gs.board[r][c][0] == ('w' if gs.whiteToMove else 'b'):
+            s = p.Surface((SQ_SIZE, SQ_SIZE))
+            s.set_alpha(50)
+            s.fill(p.Color('green'))
+            screen.blit(s, (c*SQ_SIZE, r *SQ_SIZE))
+            s.set_alpha(255)
+            s.fill(p.Color('light blue'))
+            for move in validMoves:
+                if move.startRow == r and move.startCol == c:
+                    screen.blit(s, (move.endCol * SQ_SIZE, move.endRow * SQ_SIZE))
 
-def drawGameState(screen, gs):
+def drawGameState(screen, gs, validMoves, sqSelected):
     drawBoard(screen)
+    hightlightSquares(screen, gs, validMoves, sqSelected)
     drawPieces(screen, gs.board)
+
 
 
 def drawBoard(screen):
     colors = [p.Color("white"), p.Color("gray")]
     for i in range(DIMENSION):
         for j in range(DIMENSION):
-            color = colors[((i+j) % 2)]
-            p.draw.rect(screen, color, p.Rect(j*SQ_SIZE, i*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            color = colors[((i + j) % 2)]
+            p.draw.rect(screen, color, p.Rect(j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
 
 def drawPieces(screen, board):
-    for i in range (DIMENSION):
-        for j in range (DIMENSION):
+    for i in range(DIMENSION):
+        for j in range(DIMENSION):
             piece = board[i][j]
-            if(piece != "--"):
-                screen.blit(IMAGES[piece], p.Rect(j*SQ_SIZE, i*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == "__main__":
